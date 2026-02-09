86e1e4eb834ef9cdbc9fafb81ffae6dbd9f53903
xytlw <142857@ruri.waseda.jp>
2026-02-09 22:08:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -270,6 +270,6 @@ function hanshinBraker(n) {
     tree.partitionHandler();
     exprStack.initStack();
     tree.formExpr(exprStack);
-    return exprStack.peek());
+    return exprStack.peek();
     
 }
